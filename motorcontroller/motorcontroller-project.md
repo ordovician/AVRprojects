6060e7c4366de591fe6f9662a06b280cabbcaaa0
Erik Engheim <erik.engheim@mac.com>
2013-03-25 23:02:08 +0100

    experimenting with phase correct pwm

---
+++ motorcontroller/motorcontroller-project.md
@@ -0,0 +1,11 @@
+Electric spinning wheel motor controller
+----------------------------------------
+There is already one out there based on Arduino. But I want to create one based on ATtiny13 written in assemly. Why? We only need one input for potmeter and one output for driving the motor. Thus we should not need anything more complicated than a ATtiny13 which has 6 IO pins. The program is also fairly small so it would be a fun excersise to write it in assembly rather than C. It could also serve as a good example for others who want to experiment with assembly programming.
+
+
+
+[motodriver]: http://www.electrokit.com/motordrivare-l298-dubbel-hbrygga.49762 "Dual  full-bridge motor driver"
+[electrokit]: http://www.electrokit.com "Electro:kit"
+[botdino]: http://www.lynxmotion.com/c-153-botboarduino.aspx
+[botdinobuy]: http://www.electrokit.com/en/botboarduino.50217
+[lynx]: http://www.lynxmotion.com
---
+++ motorcontroller/motorcontroller.asm
@@ -0,0 +1,78 @@
+;PWM to control a motor, at 100 Hz. 10ms period.
+;With CPU clock of 1.2e6 and prescaller 1/1024, each clock tick is about 0.8ms
+;That means we have to count to about 12 to get 10ms.
+
+.include "tn13def.inc"
+
+.def a = r16
+.def b = r17
+
+.org 0000
+	rjmp Reset			;reset
+	reti				;external interrupt 0
+	reti				;pin change interrupt 0
+	reti				;timer/counter overflow
+	reti				;eeprom ready
+	reti				;analog comparator
+	reti 				;timer/counter compare match A
+	reti				;timer/counter compare match B
+	reti				;watchdog time-out
+	rjmp ADCComplete	;ADC conversion complete
+	
+ON_RESET:
+    SBI DDRB,PB0           ;SET PORTB0 FOR OUTPUT
+    LDI a,0b10000001    ;SET TO PWM MODE 1
+    OUT TCCR0A,A
+    LDI A,0b00000101    ;SET PRESCALER/DIVIDER TO /1024        
+    OUT TCCR0B,A        
+    LDI A,200            ;DIFFERENT VALUE
+	OUT OCR0A,A        ;FOR COMPARE
+
+MAIN_LOOP: RJMP MAIN_LOOP;A DO-NOTHING LOOP
+	
+Reset:
+	ldi a, low(RAMEND)
+	out SPL, a
+	
+	;set outputs (LEDs)
+	sbi DDRB, PB0
+	sbi DDRB, PB1		;debug
+	cbi PORTB, PB0		;turn off LED
+	cbi PORTB, PB1
+	
+	;set inputs
+	cbi DDRB, PB2	;button to enable configuration of max, min, middle
+	sbi PORTB, PB2	;enable pullup for button
+	
+	;setup prescaler to 1/1024, and phase correct PWM, with variable duty cycle
+	ldi a, 1<<CS02 | 1<<CS00
+	out TCCR0B, a
+	ldi a, 1<<WGM00 | 1<<COM0A0		;Toggle OC0A on compare match
+	out TCCR0A, a
+	
+	ldi a, 128
+	out OCR0A, a
+	
+	;ADC
+	;ADc ENable, ADc Start Conversion, ADc Auto upDATE, ADc Interrupt Enable
+	; with 50-200KHz, SYS CLOCK IS 1.2MHz 
+	; ldi a, 1<<ADEN | 1<<ADSC | 1<<ADATE | 1<<ADIE | 1<<ADPS1 | 1<<ADPS0
+	; out ADCSRA, a
+	; 
+	; ;Input ADC on pin PB4/ADC2
+	; ldi a, 1<<MUX1
+	; out ADMUX, a
+	; sei	
+
+loop:
+	nop
+	rjmp loop
+
+ADCComplete:
+	in a, ADCL
+	in b, ADCH
+	out OCR0A, a
+	reti
+
+
+
